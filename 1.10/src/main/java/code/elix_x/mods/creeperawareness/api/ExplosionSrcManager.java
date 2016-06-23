620ec858f7e832daa1a510796ec7aa89fb66cec9
elix-x <agent-lego@mail.ru>
2016-06-23 15:41:34 +0200

    Updated to 1.10
    Updated to 1.10.

---
+++ 1.10/src/main/java/code/elix_x/mods/creeperawareness/api/ExplosionSrcManager.java
@@ -0,0 +1,210 @@
+package code.elix_x.mods.creeperawareness.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import code.elix_x.excore.utils.shape3d.AxisAlignedBox;
+import code.elix_x.excore.utils.shape3d.Shape3D;
+import code.elix_x.mods.creeperawareness.api.events.GetExplosionSourceFromEntityEvent;
+import code.elix_x.mods.creeperawareness.api.events.RerouteUnformalEntityEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.pathfinding.PathNavigate;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
+
+public class ExplosionSrcManager {
+
+	public static final Logger logger = LogManager.getLogger("Creeper Awareness Explosions Mananger");
+
+	public static File configFolder;
+	public static File configFile;
+	public static Configuration config;
+
+	public static boolean smartMobs = true;
+
+	public static void preInit(FMLPreInitializationEvent event){
+		configFolder = new File(event.getModConfigurationDirectory(), "Avoid Exploding Creepers");
+		configFolder.mkdir();
+		configFile = new File(configFolder, "API.cfg");
+		try {
+			configFile.createNewFile();
+		} catch (IOException e){
+			logger.error("Caught exception while creating config file: ", e);
+		}
+		config = new Configuration(configFile);
+		config.load();
+		smartMobs = config.getBoolean("smartMobs", "Mobs", true, "Mobs know vector math and calculate exact runaway position using it.\nIf false, mobs don't know vector math and run to random block away...");
+		config.save();
+	}
+
+	public static void init(FMLInitializationEvent event){
+		MinecraftForge.EVENT_BUS.register(new TickEvent());
+	}
+
+	public static void postInit(FMLPostInitializationEvent event){
+
+	}
+
+	public static void serverStopping(FMLServerStoppingEvent event){
+		entitySourceMap.clear();
+		specialSources.clear();
+		sourceEntitiesMap.clear();
+	}
+
+	public static class TickEvent {
+
+		@SubscribeEvent
+		public void onTickWorld(WorldTickEvent event){
+			if(!event.world.isRemote && event.phase == Phase.START){
+				tick(event.world);
+			}
+		}
+
+		@SubscribeEvent
+		public void onTickServer(ServerTickEvent event){
+			if(event.phase == Phase.START){
+				tick();
+			}
+		}
+
+	}
+
+	private static Map<Entity, IExplosionSource> entitySourceMap = new HashMap<Entity, IExplosionSource>();
+	private static List<IExplosionSource> specialSources = new ArrayList<IExplosionSource>();
+
+	public static void addExplosionSource(IExplosionSource source){
+		if(!entitySourceMap.containsValue(source)) specialSources.add(source);
+	}
+
+	private static void tick(){
+		Iterator<IExplosionSource> it = entitySourceMap.values().iterator();
+		while(it.hasNext()){
+			IExplosionSource source = it.next();
+			if(!source.isValid()){
+				it.remove();
+				sourceEntitiesMap.removeAll(source);
+			}
+		}
+		it = specialSources.iterator();
+		while(it.hasNext()){
+			IExplosionSource source = it.next();
+			if(!source.isValid()){
+				it.remove();
+				sourceEntitiesMap.removeAll(source);
+			}
+		}
+		for(IExplosionSource source : specialSources){
+			processSource(source);
+		}
+	}
+
+	private static void tick(World world){
+		Iterator<Entity> it = world.loadedEntityList.iterator();
+		while(it.hasNext()){
+			Entity entity = it.next();
+			IExplosionSource source = entitySourceMap.get(entity);
+			if(source != null){
+				processSource(source);
+			} else { 
+				if(entity instanceof IExplosionSource){
+					source = (IExplosionSource) entity;
+				}
+				GetExplosionSourceFromEntityEvent event = new GetExplosionSourceFromEntityEvent(entity, source);
+				MinecraftForge.EVENT_BUS.post(event);
+				source = event.explosionSource;
+
+				if(source != null){
+					entitySourceMap.put(entity, source);
+					processSource(source);
+				}
+			}
+		}
+	}
+
+	private static Multimap<IExplosionSource, Entity> sourceEntitiesMap = HashMultimap.create();
+
+	private static void processSource(IExplosionSource source){
+		if(source.isValid()){
+			if(source.update()){
+				if(source.isExploding()){
+					for(Entity entity : source.getExplosionShape().getAffectedEntities(source.getWorldObj(), Entity.class)){
+						if(entity != source.getHandledEntity() && (source.isDirty() || !sourceEntitiesMap.containsEntry(source, entity))){
+							sourceEntitiesMap.put(source, entity);
+							processEntity(source, entity);
+						}
+					}
+					source.setDirty(false);
+				}
+			}
+		}
+	}
+
+	private static void processEntity(IExplosionSource source, Entity e){
+		if(e == source.getHandledEntity()) return;
+		if(e instanceof EntityPlayer) return;
+		if(e instanceof EntityCreature){
+			EntityCreature entity = (EntityCreature) e;
+			Vec3d vec3;
+			Shape3D shape = source.getExplosionShape();
+			Vec3d sourcePos = shape.getPos();
+			if(smartMobs){
+				Vec3d ePos = new Vec3d(entity.posX, entity.posY, entity.posZ);
+				Vec3d vec = ePos.subtract(sourcePos).normalize();
+				Vec3d out = new Vec3d(0, 0, 0);
+				AxisAlignedBB box = entity.getEntityBoundingBox();
+				while(box != null ? shape.intersectsWith(source.getWorldObj(), new AxisAlignedBox(box.offset(out.xCoord, out.yCoord, out.zCoord))) : shape.isInside(source.getWorldObj(), ePos.addVector(out.xCoord, out.yCoord, out.zCoord))){
+					out = out.addVector(vec.xCoord, vec.yCoord, vec.zCoord);
+				}
+				double x = ePos.xCoord + out.xCoord;
+				double y = ePos.yCoord + out.yCoord;
+				double z = ePos.zCoord + out.zCoord;
+				for(int i = 0; source.getWorldObj().getBlockState(new BlockPos(x, y, z)).getBlock() != Blocks.AIR; i = i == 0 ? 1 : i > 0 ? -i : -i + 1){
+					y = ePos.yCoord + out.yCoord + i;
+					if(i < 0 || i > 255){
+						y = source.getWorldObj().getHeight(new BlockPos(x, y, z)).getY();
+						break;
+					}
+				}
+				vec3 = new Vec3d(x, y, z);
+			} else {
+				shape = shape.getBounds();
+				vec3 = RandomPositionGenerator.findRandomTargetBlockAwayFrom(entity, source.getExplosionRadius(), source.getExplosionRadius(), sourcePos);
+			}
+			double eb = entity.getDistanceSq(vec3.xCoord, vec3.yCoord, vec3.zCoord);
+			double speed = eb / (source.getTimeBeforeExplosion() / 2.5);
+
+			PathNavigate navigator = entity.getNavigator();
+			navigator.clearPathEntity();
+			navigator.tryMoveToXYZ(vec3.xCoord, vec3.yCoord, vec3.zCoord, speed);
+		} else {
+			MinecraftForge.EVENT_BUS.post(new RerouteUnformalEntityEvent(source, e));
+		}
+	}
+}
---
+++ 1.10/src/main/java/code/elix_x/mods/creeperawareness/events/BindTntEvent.java
@@ -0,0 +1,76 @@
+package code.elix_x.mods.creeperawareness.events;
+
+import code.elix_x.excore.utils.shape3d.Shape3D;
+import code.elix_x.excore.utils.shape3d.Sphere;
+import code.elix_x.mods.creeperawareness.api.IExplosionSource;
+import code.elix_x.mods.creeperawareness.api.events.GetExplosionSourceFromEntityEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class BindTntEvent {
+
+	@SubscribeEvent
+	public void bind(GetExplosionSourceFromEntityEvent event){
+		if(event.getEntity() instanceof EntityTNTPrimed && event.explosionSource == null){
+			final EntityTNTPrimed tnt = (EntityTNTPrimed) event.getEntity();
+			event.explosionSource = new IExplosionSource(){
+
+				private boolean dirty = true;
+
+				@Override
+				public Entity getHandledEntity(){
+					return tnt;
+				}
+
+				@Override
+				public World getWorldObj(){
+					return tnt.worldObj;
+				}
+
+				@Override
+				public boolean isExploding(){
+					return true;
+				}
+
+				@Override
+				public int getTimeBeforeExplosion(){
+					return tnt.getFuse();
+				}
+
+				@Override
+				public int getExplosionRadius(){
+					return 8;
+				}
+
+				@Override
+				public Shape3D getExplosionShape(){
+					return new Sphere(tnt.posX, tnt.posY, tnt.posZ, getExplosionRadius());
+				}
+
+				@Override
+				public boolean update(){
+					if(tnt.posX != tnt.prevPosX || tnt.posY != tnt.prevPosY || tnt.posZ != tnt.prevPosZ) setDirty(true);
+					return true;
+				}
+
+				@Override
+				public boolean isDirty(){
+					return dirty;
+				}
+
+				@Override
+				public boolean setDirty(boolean dirty){
+					return this.dirty = dirty;
+				}
+
+				@Override
+				public boolean isValid(){
+					return tnt != null && !tnt.isDead;
+				}
+
+			};
+		}
+	}
+}

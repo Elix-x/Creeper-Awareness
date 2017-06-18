920401c0debaa384fcdb3d53e0eeafc913fd53ee
elix-x <Elix-x@users.noreply.github.com>
2017-06-18 22:42:27 +0200

    Changed API version to gradle-set

--- src/main/java/code/elix_x/mods/creeperawareness/api/events/package-info.java
+++ src/main/java/code/elix_x/mods/creeperawareness/api/events/package-info.java
@@ -5,7 +5,7 @@
  * @author elix_x
  *
  */
-@API(provides = "Creeper Awareness | API | EVENTS", owner = "Creeper Awareness", apiVersion = "2.0")
+@API(provides = "Creeper Awareness | API | EVENTS", owner = "Creeper Awareness", apiVersion = "@VERSION@")
 package code.elix_x.mods.creeperawareness.api.events;
 
 import net.minecraftforge.fml.common.API;
--- src/main/java/code/elix_x/mods/creeperawareness/api/package-info.java
+++ src/main/java/code/elix_x/mods/creeperawareness/api/package-info.java
@@ -5,7 +5,7 @@
  * @author elix_x
  *
  */
-@API(provides = "Creeper Awareness | API", owner = "Creeper Awareness", apiVersion = "2.0")
+@API(provides = "Creeper Awareness | API", owner = "Creeper Awareness", apiVersion = "@VERSION@")
 package code.elix_x.mods.creeperawareness.api;
 
 import net.minecraftforge.fml.common.API;
